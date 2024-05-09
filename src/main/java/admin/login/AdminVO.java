45ecea531fc76017182ef1a7f3049884fe0bae3c
Gang517 <Gang517@gmail.com>
2024-05-09 15:12:24 +0900

    feat : Admin login 기능 구현

---
+++ src/main/java/admin/login/AdminVO.java
@@ -0,0 +1,12 @@
+package admin.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+
+public class AdminVO {
+  private String id;
+  private String password;
+}
